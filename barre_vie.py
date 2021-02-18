fe397ef2d7dc21cbb5b87e48fc98eec6d905f08f
Rachel Mountain-Manoury <rachel.mountain.manoury@gmail.com>
2021-02-18 09:43:22 +0100

    update bg et barre de vie

--- barre_vie.py
+++ barre_vie.py
@@ -8,32 +8,34 @@ pygame.init()
 #Ouverture de la fenêtre Pygame
 fenetre = pygame.display.set_mode((1920,1080))
 WHITE=(255,255,255)
+VIE=(236, 121, 250)
+MORT=(74, 34, 141)
 
 #Chargement et collage du fond
 fond = pygame.image.load("assets/bg3.png").convert()
 fenetre.blit(fond, (0,0))
-Pv_P1=100
+Pv_P1=100 # variable qui diminuera quand touché par une bombe
 Pvmax=100
-Xpos_P1=50
-Ypos_P1=20 
-LongBarVie=200
-LargBarVie=20
+Xpos_P1=330
+Ypos_P1=110
+LongBarVie=495
+LargBarVie=75
 LongBarViePerdu_P1=(Pvmax-Pv_P1)/Pvmax*LongBarVie
 XposBarViePerdu_P1=(Xpos_P1+LongBarVie)-LongBarViePerdu_P1
-pygame.draw.rect(fenetre,(0,255,0),(Xpos_P1,Ypos_P1,LongBarVie,LargBarVie))
-pygame.draw.rect(fenetre,(255,0,0),(XposBarViePerdu_P1,Ypos_P1,LongBarViePerdu_P1,LargBarVie))
+pygame.draw.rect(fenetre,VIE,(Xpos_P1,Ypos_P1,LongBarVie,LargBarVie))
+pygame.draw.rect(fenetre,MORT,(XposBarViePerdu_P1,Ypos_P1,LongBarViePerdu_P1,LargBarVie))
 
-
+# idem pour le deuxième joueur
 Pv_P2=100
 Pvmax=100
-Xpos_P2=400
-Ypos_P2=20
-LongBarVie=200
-LargBarVie=20
+Xpos_P2=1105
+Ypos_P2=110
+LongBarVie=495
+LargBarVie=75
 LongBarViePerdu_P2=(Pvmax-Pv_P2)/Pvmax*LongBarVie
 XposBarViePerdu_P2=(Xpos_P1+LongBarVie)-LongBarViePerdu_P2
-pygame.draw.rect(fenetre,(0,255,0),(Xpos_P2,Ypos_P2,LongBarVie,LargBarVie))
-pygame.draw.rect(fenetre,(255,0,0),(XposBarViePerdu_P2,Ypos_P2,LongBarViePerdu_P2,LargBarVie))
+pygame.draw.rect(fenetre,VIE,(Xpos_P2,Ypos_P2,LongBarVie,LargBarVie))
+pygame.draw.rect(fenetre,MORT,(XposBarViePerdu_P2,Ypos_P2,LongBarViePerdu_P2,LargBarVie))
 
 #Rafraîchissement de l'écran
 pygame.display.flip()
