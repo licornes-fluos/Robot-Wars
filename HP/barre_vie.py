416d943d54a6104ef9936aaf147d525afc5603dd
Rachel Mountain-Manoury <rachel.mountain.manoury@gmail.com>
2021-03-10 11:21:07 +0100

    couleurs barres de vie

--- HP/barre_vie.py
+++ HP/barre_vie.py
@@ -7,7 +7,7 @@ class Barre_vie():
 
     #Ouverture de la fenêtre Pygame
 
-    def __init__(self, pv, pvmax, xpos, ypos, longu, larg):
+    def __init__(self, pv, pvmax, xpos, ypos, longu, larg, c_vie, c_mort):
         super().__init__()
 
         self.pv = pv
@@ -16,12 +16,14 @@ class Barre_vie():
         self.ypos = ypos
         self.longu = longu
         self.larg = larg
-        self.long_perdu=(pvmax-pv)/pvmax*longu
-        self.xpos_perdu=(xpos+longu)-self.long_perdu
+        self.long_perdu = (pvmax-pv)/pvmax*longu
+        self.xpos_perdu = (xpos+longu)-self.long_perdu
+        self.c_vie = c_vie
+        self.c_mort = c_mort 
 
     def barres(self, fenetre):
         WHITE=(255,255,255)
         VIE=(236, 121, 250)
         MORT=(74, 34, 141)
-        pygame.draw.rect(fenetre,VIE,(self.xpos,self.ypos,self.longu,self.larg))
-        pygame.draw.rect(fenetre,MORT,(self.xpos_perdu,self.ypos,self.long_perdu,self.larg))
+        pygame.draw.rect(fenetre,self.c_vie,(self.xpos,self.ypos,self.longu,self.larg))
+        pygame.draw.rect(fenetre,self.c_mort,(self.xpos_perdu,self.ypos,self.long_perdu,self.larg))
